0b1bc2add8fa4905c439ec73560b1c4d6b13af0a
mark79-github <52877024+mark79-github@users.noreply.github.com>
2022-10-11 09:45:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidapplicationbasics/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.androidapplicationbasics
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidapplicationbasics.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val stack = ArrayDeque<String>()
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSubmit.setOnClickListener() {
+            submit()
+        }
+
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
+
+        binding.btnUndo.setOnClickListener {
+            undo()
+        }
+    }
+
+    private fun submit() {
+        val enteredText = binding.editText.text.trim().toString()
+        if (enteredText.isEmpty()) {
+            showToastMessage(getString(R.string.msg_enter_text))
+            return
+        }
+
+        val text = getString(
+            R.string.text_view_text, stack.size + 1, enteredText
+        )
+
+        binding.textView.text = text
+        binding.editText.setText("")
+        stack.addLast(enteredText)
+
+        showToastMessage(getString(R.string.msg_submit_text, enteredText))
+    }
+
+    private fun reset() {
+        stack.clear()
+        binding.textView.text = ""
+        binding.editText.setText("")
+
+        showToastMessage(getString(R.string.msg_reset_text))
+    }
+
+    private fun undo() {
+        if (stack.isEmpty()) {
+            binding.editText.setText("")
+            showToastMessage(getString(R.string.msg_undo_no_elements_text))
+            return
+        }
+
+        binding.textView.text = ""
+        val lastElement = stack.removeLast()
+        binding.editText.setText(lastElement)
+        showToastMessage(getString(R.string.msg_undo_undo_text, lastElement))
+    }
+
+    private fun showToastMessage(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+    }
+}
